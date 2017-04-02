5fb2e04d9be269a1b71f26610731161cf0a5dcd8
Thomas SIMOES <simoes_t@MacBook-Pro-de-Thomas.local>
2017-04-02 13:16:24 +0200

    up

--- src/jquery/app/Main.java
+++ src/jquery/app/Main.java
@@ -21,10 +21,10 @@ public class Main {
 			user2.setAttribute(new Attribute("id",  "2"));
 			JQuery("users").append(user1);
 			JQuery("users").append(user2);
-			JQuery("user[id=1]").append(new Element("nom").setText("simoes"));
-			JQuery("user[id=1]").append(new Element("prenom").setText("thomas"));
-			JQuery("user[id=2]").append(new Element("nom").setText("simoes"));
-			JQuery("user[id=2]").append(new Element("prenom").setText("lea"));
+			JQuery("user[id=1]").append(new Element("nom").setText("bidule"));
+			JQuery("user[id=1]").append(new Element("prenom").setText("chouette"));
+			JQuery("user[id=2]").append(new Element("nom").setText("pipi"));
+			JQuery("user[id=2]").append(new Element("prenom").setText("prout"));
 			output(writer);
 			close(writer);
 		}
